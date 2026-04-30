d32ff0948f1604c72145bc14aad12cca8a201e63
elshoto2000 <herreraleandro628@gmail.com>
2026-04-29 22:32:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,23 +32,22 @@ def home():
 
 @app.route('/juego/<nombre_juego>')
 def cargar_juego(nombre_juego):
-    # Agarramos el usuario de la URL (ej: /juego/snake?user=Leandro)
-    nombre_usuario = request.args.get('user', 'Invitado')
-    
+    # Intentamos obtener el usuario de la URL por si acaso reintentan
+    nombre_usuario = request.args.get('user', '')
+
     conn = conectar_db()
     # Sacamos el Top 5 de cada juego por separado
-    top_snake = conn.execute('SELECT nombre, puntos FROM ranking WHERE juego = "Snake" ORDER BY puntos DESC LIMIT 5').fetchall()
-    top_trivia = conn.execute('SELECT nombre, puntos FROM ranking WHERE juego = "Trivia" ORDER BY puntos DESC LIMIT 5').fetchall()
-    top_clicker = conn.execute('SELECT nombre, puntos FROM ranking WHERE juego = "Clicker" ORDER BY puntos DESC LIMIT 5').fetchall()
+    r_snake = conn.execute('SELECT nombre, puntos FROM ranking WHERE juego = "Snake" ORDER BY puntos DESC LIMIT 5').fetchall()
+    r_trivia = conn.execute('SELECT nombre, puntos FROM ranking WHERE juego = "Trivia" ORDER BY puntos DESC LIMIT 5').fetchall()
+    r_clicker = conn.execute('SELECT nombre, puntos FROM ranking WHERE juego = "Clicker" ORDER BY puntos DESC LIMIT 5').fetchall()
     conn.close()
 
-    # IMPORTANTE: Enviamos todo al HTML
     return render_template('juego.html', 
                            juego=nombre_juego, 
                            usuario=nombre_usuario,
-                           ranking_snake=top_snake,
-                           ranking_trivia=top_trivia,
-                           ranking_clicker=top_clicker)
+                           ranking_snake=r_snake,
+                           ranking_trivia=r_trivia,
+                           ranking_clicker=r_clicker)
 
 # Tu ruta de guardar_puntaje está perfecta con la lógica de UPSERT
 @app.route('/guardar_puntaje', methods=['POST'])
